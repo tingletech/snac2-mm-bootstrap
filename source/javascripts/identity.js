e63e818f547b47d2725a335fd4a697f50a6df625
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2014-04-25 21:35:04 -0700

    enable icons on identity page

--- source/javascripts/identity.js
+++ source/javascripts/identity.js
@@ -82,6 +82,8 @@ $(function() {
         }
     });
 })();
+window.grunticon=function(e){if(e&&3===e.length){var t=window,n=!!t.document.createElementNS&&!!t.document.createElementNS("http://www.w3.org/2000/svg","svg").createSVGRect&&!!document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image","1.1"),A=function(A){var o=t.document.createElement("link"),r=t.document.getElementsByTagName("script")[0];o.rel="stylesheet",o.href=e[A&&n?0:A?1:2],r.parentNode.insertBefore(o,r)},o=new t.Image;o.onerror=function(){A(!1)},o.onload=function(){A(1===o.width&&1===o.height)},o.src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw=="}};
+grunticon( [ "../stylesheets/grunticon/icons.data.svg.css", "../stylesheets/grunticon/icons.data.png.css", "../stylesheets/grunticon/icons.fallback.css" ] );
 /* # License 
 
 Copyright © 2014, Regents of the University of California
