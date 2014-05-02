5e7bfe0f3b51dbb35973ab14e314e72f7cc833d5
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2014-05-01 18:01:00 -0700

    hardcode icon css paths for now

--- source/javascripts/results.js
+++ source/javascripts/results.js
@@ -240,4 +240,4 @@ $(function () {
 });
 
 window.grunticon=function(e){if(e&&3===e.length){var t=window,n=!!t.document.createElementNS&&!!t.document.createElementNS("http://www.w3.org/2000/svg","svg").createSVGRect&&!!document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image","1.1"),A=function(A){var o=t.document.createElement("link"),r=t.document.getElementsByTagName("script")[0];o.rel="stylesheet",o.href=e[A&&n?0:A?1:2],r.parentNode.insertBefore(o,r)},o=new t.Image;o.onerror=function(){A(!1)},o.onload=function(){A(1===o.width&&1===o.height)},o.src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw=="}};
-grunticon( [ "../stylesheets/grunticon/icons.data.svg.css", "../stylesheets/grunticon/icons.data.png.css", "../stylesheets/grunticon/icons.fallback.css" ] );
+grunticon( [ "http://bioghist.s3-website-us-east-1.amazonaws.com/stylesheets/grunticon/icons.data.svg.css", "http://bioghist.s3-website-us-east-1.amazonaws.com/stylesheets/grunticon/icons.data.png.css", "http://bioghist.s3-website-us-east-1.amazonaws.com/stylesheets/grunticon/icons.fallback.css" ] );
